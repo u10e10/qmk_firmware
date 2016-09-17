395600aa554172bbe43e4cc95fb33ce48eeac2cd
u10e10 <uplus.e10@gmail.com>
2016-09-18 01:34:28 +0900

    Update config.

--- keyboards/ergodox/ez/config.h
+++ keyboards/ergodox/ez/config.h
@@ -21,6 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "../config.h"
 
+#define ONESHOT_TIMEOUT 1000
+
+
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x1307
