d41e9396556e937a6d3277b3aaccea658a163be4
u10e10 <uplus.e10@gmail.com>
2016-09-19 22:20:16 +0900

    Change lang map and backspace position

--- keyboards/ergodox/ez/keymaps/uplus/keymap.c
+++ keyboards/ergodox/ez/keymaps/uplus/keymap.c
@@ -30,11 +30,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   L3   |      |      |      |      |                                 |   (  |   )  |   [  |   ]  |        |
  * `------------------------------------'                                 `------------------------------------'
  *                                     ,--------------.     ,--------------.
- *                                     |BS/Alt | App  |     |      |Esc/Alt|
+ *                                     |Eng/Alt| App  |     |      |Jpn/Alt|
  *                              ,------|-------|------|     |------+-------+-------.
  *                              |      |       | Del  |     |      | Alt/  |Enter/ |
  *                              |Space |LShift |------|     |------| Space |  L1   |
- *                              |      |       | Eng  |     | Jp   |       |       |
+ *                              |      |       |  BS  |     | ESC  |       |       |
  *                              `---------------------'     `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -45,18 +45,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,        KC_A,      KC_S,   KC_D,   KC_F,   KC_G,
     GUI_T(KC_ESC),  KC_Z,      KC_X,   KC_C,   KC_V,   KC_B,   KC_GRV,
     MO(LHND),       KC_NO,     KC_NO,  KC_NO,  KC_NO,
-                                               ALT_T(KC_BSPC), KC_APP,
+                                               ALT_T(KC_LANG1),KC_APP,
                                                                KC_DELT,
-                               KC_SPC,         OSM(MOD_LSFT),  KC_LANG1,
+                               KC_SPC,         OSM(MOD_LSFT),  KC_BSPC,
 
     KC_NO,       KC_6,   KC_7,     KC_8,    KC_9,   KC_0,             KC_NO,
     KC_MINS,     KC_Y,   KC_U,     KC_I,    KC_O,   KC_P,             KC_BSLS,
                  KC_H,   KC_J,     KC_K,    KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
     KC_EQL,      KC_N,   KC_M,     KC_COMM, KC_DOT, CTL_T(KC_SLSH),   OSM(MOD_RSFT),
                          KC_LPRN,  KC_RPRN, KC_LBRC, KC_RBRC,KC_NO,
-    KC_NO,       ALT_T(KC_ESC),
+    KC_NO,       ALT_T(KC_LANG2),
     KC_NO,
-    KC_LANG2,    ALT_T(KC_SPC),    LT(SYMB, KC_ENT)
+    KC_ESC,      ALT_T(KC_SPC),    LT(SYMB, KC_ENT)
     ),
 
 
