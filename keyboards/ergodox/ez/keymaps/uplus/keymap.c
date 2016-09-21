c233e5299a7380def474166786c82a2dbe0518b8
u10e10 <uplus.e10@gmail.com>
2016-09-21 23:39:08 +0900

    Update left hand layer

--- keyboards/ergodox/ez/keymaps/uplus/keymap.c
+++ keyboards/ergodox/ez/keymaps/uplus/keymap.c
@@ -138,31 +138,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|     |------+------+------+------+------+------+--------|
  * |        |  F7  |  F8  |  F9  | F10  | F11  | F12  |     |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |------|     |------|      |      |      |      |      |        |
+ * |        |      |      |Enter |BSpace|Delete|------|     |------|      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |     |      |      |      |      |      |      |        |
+ * |        |      |      |WBack | PgUp | WFwd |      |     |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'     `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                 |      |      |      |      |      |
+ *   |      |      |      |      | PgDn |                                 |      |      |      |      |      |
  *   `----------------------------------'                                 `----------------------------------'
- *                                      ,-------------.     ,-------------.
- *                                      |      |      |     |      |      |
- *                               ,------|------|------|     |------+------+------.
- *                               |      |      |      |     |      |      |      |
- *                               | PgUp | PgDn |------|     |------|      |      |
- *                               |      |      |      |     |      |      |      |
- *                               `--------------------'     `--------------------'
  */
 // Left hand layer
 [LHND] = KEYMAP(
        // left hand
        KC_TRNS,   KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,
        KC_TRNS,   KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,
-       KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-       KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-       KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+       KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_ENT,    KC_BSPC,   KC_DELT,
+       KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_WBAK,   KC_PGUP,   KC_WFWD,   KC_TRNS,
+       KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_PGDN,
                                                               KC_TRNS,   KC_TRNS,
                                                                          KC_TRNS,
-                                                     KC_PGUP, KC_PGDN,   KC_TRNS,
+                                                     KC_NO,   KC_NO,     KC_TRNS,
        // right hand
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
