7d6ded0eab1cde3d8caaeb612296f27fba92ca77
u10e10 <uplus.e10@gmail.com>
2016-10-30 19:31:08 +0900

    Add new Backspace and GRV

--- keyboards/ergodox/ez/keymaps/uplus/keymap.c
+++ keyboards/ergodox/ez/keymaps/uplus/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.     ,--------------------------------------------------.
  * |   L3   |   1  |   2  |   3  |   4  |   5  |      |     |      |   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+-------------|     |------+------+------+------+------+------+--------|
- * |Tab/Hyp |   Q  |   W  |   E  |   R  |   T  |      |     |  -   |   Y  |   U  |   I  |   O  |   P  |        |
+ * |Tab/Hyp |   Q  |   W  |   E  |   R  |   T  |  BS  |     |  -   |   Y  |   U  |   I  |   O  |   P  |   `    |
  * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|     |------|   H  |   J  |   K  |   L  | ;/ L2| '/L3   |
  * |--------+------+------+------+------+------|  `   |     |  =   |------+------+------+------+------+--------|
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
     MO(MISC),       KC_1,      KC_2,   KC_3,   KC_4,   KC_5,   KC_NO,
-    ALL_T(KC_TAB),  KC_Q,      KC_W,   KC_E,   KC_R,   KC_T,   KC_NO,
+    ALL_T(KC_TAB),  KC_Q,      KC_W,   KC_E,   KC_R,   KC_T,   KC_BSPC,
     KC_LCTL,        KC_A,      KC_S,   KC_D,   KC_F,   KC_G,
     GUI_T(KC_ESC),  LT_3(Z),   KC_X,   KC_C,   KC_V,   KC_B,   KC_GRV,
     KC_NO,          KC_GRV,    KC_LPRN,KC_RPRN,KC_LANG1,
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_SPC,         OSM(MOD_LSFT),  KC_LALT,
 
     KC_NO,       KC_6,   KC_7,     KC_8,    KC_9,    KC_0,           KC_NO,
-    KC_MINS,     KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,           KC_NO,
+    KC_MINS,     KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,           KC_GRV,
                  KC_H,   KC_J,     KC_K,    KC_L,    LT_2(SCLN),     LT_3(QUOT),
     KC_EQL,      KC_N,   KC_M,     KC_COMM, KC_DOT,  CTL_T(KC_SLSH), SFT_T(KC_BSLS),
                          KC_LANG2, KC_LBRC, KC_RBRC, KC_RGUI,        KC_NO,
