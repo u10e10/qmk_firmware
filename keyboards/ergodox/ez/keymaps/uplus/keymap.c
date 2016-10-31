999e70536e2769a9d9ad49c1c05a9363611b37c2
u10e10 <uplus.e10@gmail.com>
2016-11-01 02:04:54 +0900

    Cleanup

--- keyboards/ergodox/ez/keymaps/uplus/keymap.c
+++ keyboards/ergodox/ez/keymaps/uplus/keymap.c
@@ -50,14 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
-    MO(MISC),       KC_1,      KC_2,   KC_3,   KC_4,   KC_5,   KC_NO,
-    ALL_T(KC_TAB),  KC_Q,      KC_W,   KC_E,   KC_R,   KC_T,   KC_BSPC,
-    KC_LCTL,        KC_A,      KC_S,   KC_D,   KC_F,   KC_G,
-    GUI_T(KC_ESC),  LT_3(Z),   KC_X,   KC_C,   KC_V,   KC_B,   KC_GRV,
-    KC_NO,          KC_GRV,    KC_LPRN,KC_RPRN,KC_LANG1,
-                                               KC_NO,          LALT(KC_F4),
+    MO(MISC),       KC_1,     KC_2,    KC_3,    KC_4,   KC_5,  KC_NO,
+    ALL_T(KC_TAB),  KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,  KC_BSPC,
+    KC_LCTL,        KC_A,     KC_S,    KC_D,    KC_F,   KC_G,
+    GUI_T(KC_ESC),  LT_3(Z),  KC_X,    KC_C,    KC_V,   KC_B,  KC_GRV,
+    KC_NO,          KC_GRV,   KC_LPRN, KC_RPRN, KC_LANG1,
+                                                KC_NO,         LALT(KC_F4),
                                                                KC_APP,
-                               KC_SPC,         OSM(MOD_LSFT),  KC_LALT,
+                                       KC_SPC,  OSM(MOD_LSFT), KC_LALT,
 
     KC_NO,       KC_6,   KC_7,     KC_8,    KC_9,    KC_0,           KC_NO,
     KC_MINS,     KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,           KC_GRV,
