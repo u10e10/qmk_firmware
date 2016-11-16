baea4364a0eab6cbfc5dc1c35a78ea9477b10ab3
u10e10 <uplus.e10@gmail.com>
2016-11-16 23:35:16 +0900

    Change lang and prn pos

--- keyboards/ergodox/ez/keymaps/uplus/keymap.c
+++ keyboards/ergodox/ez/keymaps/uplus/keymap.c
@@ -57,13 +57,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  `   |     |  =   |------+------+------+------+------+--------|
  * |Esc/Gui | Z/L3 |   X  |   C  |   V  |   B  |      |     |      |   N  |   M  |   ,  |   .  |//Ctrl| \/SFT  |
  * |--------+------+------+------+------+-------------'     `-------------+------+------+------+------+--------|
- * |        |   `  |   (  |   )  | Eng  |                                 |  Jpn |   [  |   ]  | Gui  |        |
+ * |        | Del  |      |   (  |   )  |                                 |   [  |   ]  |      | Gui  |        |
  * `------------------------------------'                                 `------------------------------------'
  *                                     .--------------.     .--------------.
- *                                     |       |AltF4 |     | Play |ESC/Alt|
+ *                                     |  Eng  |AltF4 |     | Play |  Jpn  |
  *                              ,------|-------|------|     |------|-------|-------.
  *                              |      |       | App  |     | Del  |  L1/  | Alt/  |
- *                              |Space |LShift |------|     |------| Space | Enter |
+ *                              |Space | Shift |------|     |------| Space | Enter |
  *                              |      |       | Alt  |     | Jpn  |       |       |
  *                              `---------------------'     `----------------------'
  */
@@ -74,8 +74,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ALL_T(KC_TAB),  KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,  KC_BSPC,
     KC_LCTL,        KC_A,     KC_S,    KC_D,    KC_F,   KC_G,
     GUI_T(KC_ESC),  LT_3(Z),  KC_X,    KC_C,    KC_V,   KC_B,  KC_GRV,
-    XXXXXXX,        KC_GRV,   KC_LPRN, KC_RPRN, KC_LANG1,
-                                                XXXXXXX,       LALT(KC_F4),
+    XXXXXXX,        KC_DELT,  XXXXXXX, KC_LPRN, KC_RPRN,
+                                                KC_LANG1,      LALT(KC_F4),
                                                                KC_APP,
                                        KC_SPC,  OSM(MOD_LSFT), KC_LALT,
 
@@ -83,8 +83,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_MINS,     KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,           KC_GRV,
                  KC_H,   KC_J,     KC_K,    KC_L,    LT_2(SCLN),     LT_3(QUOT),
     KC_EQL,      KC_N,   KC_M,     KC_COMM, KC_DOT,  CTL_T(KC_SLSH), SFT_T(KC_BSLS),
-                         KC_LANG2, KC_LBRC, KC_RBRC, KC_RGUI,        XXXXXXX,
-    KC_MPLY,     ALT_T(KC_ESC),
+                         KC_LBRC,  KC_RBRC, XXXXXXX, KC_RGUI,        XXXXXXX,
+    KC_MPLY,     KC_LANG2,
     KC_DELT,
     KC_LANG2,    LT(SYMB, KC_SPC), ALT_T(KC_ENT)
   ),
