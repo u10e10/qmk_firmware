bd3190005d153f7d3e80bbf13346ee3933ee8b69
u10e10 <uplus.e10@gmail.com>
2016-11-17 00:27:36 +0900

    Fix LED and comment

--- keyboards/ergodox/ez/keymaps/uplus/keymap.c
+++ keyboards/ergodox/ez/keymaps/uplus/keymap.c
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
  * |        | Lclk |MsLeft|MsDown|MsRght| WhUp |------|     |------| H_A  | H_B  | H_C  | H_D  |  **  |        |
  * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
- * |        | Rclk |WHLeft|      |WhRght|WhDown|      |     |      | MsA0 | Prev | Play | Next |      |        |
+ * |        | Rclk |WhLeft|      |WhRght|WhDown|      |     |      | MsA0 | Prev | Play | Next |      |        |
  * `--------------------------------------------------'     `--------------------------------------------------'
  */
 [MDIA] = KEYMAP(
@@ -260,22 +260,21 @@ void matrix_init_user(void) {
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
-
     uint8_t layer = biton32(layer_state);
-
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
     switch (layer) {
         case 1:
-            ergodox_right_led_2_on();
+            ergodox_right_led_1_on();
             break;
         case 2:
-            ergodox_right_led_3_on();
+            ergodox_right_led_2_on();
             break;
         case 3:
-            ergodox_right_led_1_on();
+            ergodox_right_led_3_on();
+            break;
         default:
             break;
     }
