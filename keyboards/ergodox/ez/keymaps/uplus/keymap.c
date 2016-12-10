9919f166518cb56b26c4c1722529898ef7101c89
u10e10 <uplus.e10@gmail.com>
2016-12-10 23:12:51 +0900

    Update Symbol layer

--- keyboards/ergodox/ez/keymaps/uplus/keymap.c
+++ keyboards/ergodox/ez/keymaps/uplus/keymap.c
@@ -93,27 +93,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Keymap 1: Symbol layer
  * ,--------------------------------------------------.     ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |     |      |  F6  |  F7  |  F8  |  F9  | F10  |        |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |     |      |  F6  |  F7  |  F8  |  F9  | F10  |  F12   |
  * |--------+------+------+------+------+------+------|     |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   %  |      |     |  _   |   ^  |  &   |  -   |  =   | F11  |  F12   |
+ * |        |  !   |  @   |  #   |  $   |  %   |      |     |  _   |  ^   |  &   |  *   |  (   |  )   |  F11   |
  * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
- * |        |  BS  | DEL  |   (  |   )  |   ~  |------|     |------| Left | Down |  Up  | Right|  :   |   "    |
+ * |        |  BS  | DEL  |   {  |   }  |  ~   |------|     |------| Left | Down |  Up  | Right|  :   |   "    |
  * |--------+------+------+------+------+------|  ~   |     |  +   |------+------+------+------+------+--------|
- * |        |   ^  |   $  |   [  |   ]  |   `  |      |     |      | Home | PgDn | PgUp | End  |  ?   |   |    |
+ * |        |   ^  |   $  |   [  |   ]  |      |      |     |      | Home | PgDn | PgUp | End  |  ?   |   |    |
  * `--------------------------------------------------'     `--------------------------------------------------'
  */
 [SYMB] = KEYMAP(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   _______,
-    _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR,  KC_PERC, _______,
-    _______, KC_BSPC, KC_DELT, KC_LPRN, KC_RPRN,  KC_TILD,
-    _______, KC_CIRC, KC_DLR,  KC_LBRC, KC_RBRC,  KC_GRV,  KC_TILD,
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC, _______,
+    _______, KC_BSPC, KC_DELT, KC_LCBR, KC_RCBR,  KC_TILD,
+    _______, KC_CIRC, KC_DLR,  KC_LBRC, KC_RBRC,  _______,  KC_TILD,
     _______, _______, _______, _______, _______,
                                                   _______, _______,
                                                            _______,
                                         _______,  _______, _______,
 
-    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  XXXXXXX,
-    KC_UNDS, KC_CIRC, KC_AMPR, KC_MINS, KC_EQL,   KC_F11,  KC_F12,
+    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F12,
+    KC_UNDS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, KC_F11,
              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_COLN, KC_DQUO,
     KC_PLUS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_QUES, KC_PIPE,
                       _______, _______, _______,  _______, _______,
