21446f3157806a2d7420ef4fe3e977e5ebeff657
u10e10 <uplus.e10@gmail.com>
2016-12-11 23:04:00 +0900

    Update

--- keyboards/ergodox/ez/keymaps/uplus/keymap.c
+++ keyboards/ergodox/ez/keymaps/uplus/keymap.c
@@ -28,6 +28,7 @@ enum planck_keycodes {
 #define MDIA 2 // media keys
 #define MISC 3 // left hand layer
 
+#define LOCK RGUI(KC_L)
 #define LT_2(key) LT(MDIA, KC_##key)
 #define LT_3(key) LT(MISC, KC_##key)
 #define H(key)  HYPR(KC_##key)
@@ -52,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.     ,--------------------------------------------------.
  * |   L3   |   1  |   2  |   3  |   4  |   5  |DYN_R1|     |DYN_P1|   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+------+------|     |------+------+------+------+------+------+--------|
- * |Tab/Hyp |   Q  |   W  |   E  |   R  |   T  | TAB  |     |  -   |   Y  |   U  |   I  |   O  |   P  |   BS   |
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  | TAB  |     |  -   |   Y  |   U  |   I  |   O  |   P  |   BS   |
  * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
  * |Ent/Alt |   A  |   S  |   D  |   F  |   G  |------|     |------|   H  |   J  |   K  |   L  | ;/L2 | '/Ctrl |
  * |--------+------+------+------+------+------|  `   |     |  =   |------+------+------+------+------+--------|
@@ -72,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
     MO(MISC),       KC_1,     KC_2,    KC_3,    KC_4,   KC_5,  DYN_REC1,
-    ALL_T(KC_TAB),  KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,  KC_TAB,
+    KC_TAB,         KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,  KC_TAB,
     ALT_T(KC_ENT),  KC_A,     KC_S,    KC_D,    KC_F,   KC_G,
     LT_3(ESC),      KC_Z,     KC_X,    KC_C,    KC_V,   KC_B,  KC_GRV,
     XXXXXXX,        KC_LGUI,  KC_DELT, KC_LPRN, KC_RPRN,
@@ -125,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Keymap 2: Mouse and Media layer(H_ keys are VLC's shortcuts)
  * ,--------------------------------------------------.     ,--------------------------------------------------.
- * | RESET  |      |      |      |      |      |      |     |Sleep |      |      |      |      |PrtSc |        |
+ * | RESET  |      |      |      |      |      |      |     |Sleep | Lock |      |      |PrtSc |      |        |
  * |--------+------+------+------+------+------+------|     |------+------+------+------+------+------+--------|
  * |        |      |WBack | MUp  | WFwd |      |      |     |      | H_E  |VolDn | Mute |VolUp |      |        |
  * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
@@ -144,7 +145,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                              _______,
                                            _______, _______, _______,
 
-       KC_SLEP, _______, _______, _______, _______, KC_PSCR, _______,
+       KC_SLEP, LOCK,    _______, _______, KC_PSCR, _______, _______,
        _______, H(E),    KC_VOLD, KC_MUTE, KC_VOLU, _______, _______,
                 H(A),    H(B),    H(C),    H(D),    _______, _______,
        _______, KC_ACL0, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
@@ -162,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        |  F7  |  F8  |  F9  | F10  | F11  | F12  |     |      |CA_Lft|CA_Dwn|CA_Up |CA_Rht|      |        |
  * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
  * |        |  BS  | DEL  |WBack | PgUp | WFwd |------|     |------|GA_Lft|GA_Dwn|GA_Up |GA_Rht|      |        |
- * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|AltF4 |     |      |------+------+------+------+------+--------|
  * |   **   | Prev | Play | Next | PgDn |      |      |     |      |G_Lft |G_Dwn | G_Up |G_Rht |      |        |
  * `--------+------+------+------+------+-------------'     `-------------+------+------+------+------+--------'
  */
@@ -170,7 +171,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
        _______, KC_BSPC, KC_DELT, KC_WBAK, KC_PGUP, KC_WFWD,
-       _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_PGDN, _______, _______,
+       _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_PGDN, _______, LALT(KC_F4),
        _______, _______, _______, _______, _______,
                                                     _______, _______,
                                                              _______,
