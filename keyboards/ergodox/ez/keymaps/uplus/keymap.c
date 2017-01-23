f79856e1d9866fa15574bc74fb21df46fd90adb8
u10e10 <uplus.e10@gmail.com>
2017-01-23 23:30:46 +0900

    Cleanup

--- keyboards/ergodox/ez/keymaps/uplus/keymap.c
+++ keyboards/ergodox/ez/keymaps/uplus/keymap.c
@@ -72,25 +72,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
-[BASE] = KEYMAP(  // layer 0 : default
-    MO(MISC),       KC_1,     KC_2,    KC_3,    KC_4,   KC_5,  DYN_REC1,
-    KC_TAB,         KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,  KC_TAB,
-    ALT_T(KC_ENT),  KC_A,     KC_S,    KC_D,    KC_F,   KC_G,
-    LT_3(ESC),      KC_Z,     KC_X,    KC_C,    KC_V,   KC_B,  KC_GRV,
-    XXXXXXX,        KC_LGUI,  KC_LALT, KC_ESC,  MO(MISC),
-                                                KC_LANG1,      KC_MPLY,
-                                                               KC_LANG1,
-                                       KC_LCTL, OSM(MOD_LSFT), KC_LANG1,
+  [BASE] = KEYMAP(  // layer 0 : default
+      MO(MISC),       KC_1,     KC_2,    KC_3,    KC_4,   KC_5,  DYN_REC1,
+      KC_TAB,         KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,  KC_TAB,
+      ALT_T(KC_ENT),  KC_A,     KC_S,    KC_D,    KC_F,   KC_G,
+      LT_3(ESC),      KC_Z,     KC_X,    KC_C,    KC_V,   KC_B,  KC_GRV,
+      XXXXXXX,        KC_LGUI,  KC_LALT, KC_ESC,  MO(MISC),
+      KC_LANG1,      KC_MPLY,
+      KC_LANG1,
+      KC_LCTL, OSM(MOD_LSFT), KC_LANG1,
 
-    DYN_PLY1,    KC_6,   KC_7,     KC_8,    KC_9,    KC_0,           XXXXXXX,
-    KC_MINS,     KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,           KC_BSPC,
-                 KC_H,   KC_J,     KC_K,    KC_L,    LT_2(SCLN),     CTL_T(KC_QUOT),
-    KC_EQL,      KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH,        SFT_T(KC_BSLS),
-                         KC_LBRC,  KC_RBRC, KC_RALT, KC_RGUI,        XXXXXXX,
-    KC_APP,      KC_LANG2,
-    KC_LANG2,
-    KC_LANG2,    KC_SPC, MO(SYMB)
-  ),
+      DYN_PLY1,    KC_6,   KC_7,     KC_8,    KC_9,    KC_0,           XXXXXXX,
+      KC_MINS,     KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,           KC_BSPC,
+      KC_H,   KC_J,     KC_K,    KC_L,    LT_2(SCLN),     CTL_T(KC_QUOT),
+      KC_EQL,      KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH,        SFT_T(KC_BSLS),
+      KC_LBRC,  KC_RBRC, KC_RALT, KC_RGUI,        XXXXXXX,
+      KC_APP,      KC_LANG2,
+      KC_LANG2,
+      KC_LANG2,    KC_SPC, MO(SYMB)
+      ),
 
 
 /* Keymap 1: Symbol layer
@@ -104,26 +104,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        |   ^  |   $  |   [  |   ]  |      |      |     |      | Home | PgDn | PgUp | End  |  ?   |   |    |
  * `--------------------------------------------------'     `--------------------------------------------------'
  */
-[SYMB] = KEYMAP(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   _______,
-    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC, _______,
-    _______, KC_BSPC, KC_DELT, KC_LCBR, KC_RCBR,  KC_TILD,
-    _______, KC_CIRC, KC_DLR,  KC_LBRC, KC_RBRC,  XXXXXXX,  KC_TILD,
-    _______, _______, _______, _______, _______,
-                                                  _______, _______,
-                                                           _______,
-                                        _______,  _______, _______,
+  [SYMB] = KEYMAP(
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   _______,
+      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC, _______,
+      _______, KC_BSPC, KC_DELT, KC_LCBR, KC_RCBR,  KC_TILD,
+      _______, KC_CIRC, KC_DLR,  KC_LBRC, KC_RBRC,  XXXXXXX,  KC_TILD,
+      _______, _______, _______, _______, _______,
+      _______, _______,
+      _______,
+      _______,  _______, _______,
 
-    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F12,
-    KC_UNDS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, KC_F11,
-             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_COLN, KC_DQUO,
-    KC_PLUS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_QUES, KC_PIPE,
-                      _______, _______, _______,  _______, _______,
-    _______, _______,
-    _______,
-    _______, _______, _______
-  ),
-
+      _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F12,
+      KC_UNDS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, KC_F11,
+      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_COLN, KC_DQUO,
+      KC_PLUS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_QUES, KC_PIPE,
+      _______, _______, _______,  _______, _______,
+      _______, _______,
+      _______,
+      _______, _______, _______
+      ),
 
 /* Keymap 2: Mouse and Media layer(H_ keys are VLC's shortcuts)
  * ,--------------------------------------------------.     ,--------------------------------------------------.
@@ -138,25 +137,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                                          .-------------.
  *                                                          | ACL0 | ACL0 |
  */
-[MDIA] = KEYMAP(
-       RESET,   _______, _______, _______, _______, _______, _______,
-       _______, G(R),    KC_WBAK, KC_MS_U, KC_WFWD, G(M),    _______,
-       _______, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_U,
-       _______, KC_BTN2, KC_WH_L, _______, KC_WH_R, KC_WH_D, _______,
-       _______, _______, _______, _______, _______,
-                                                    _______, _______,
-                                                             _______,
-                                           _______, _______, _______,
+  [MDIA] = KEYMAP(
+      RESET,   _______, _______, _______, _______, _______, _______,
+      _______, G(R),    KC_WBAK, KC_MS_U, KC_WFWD, G(M),    _______,
+      _______, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_U,
+      _______, KC_BTN2, KC_WH_L, _______, KC_WH_R, KC_WH_D, _______,
+      _______, _______, _______, _______, _______,
+      _______, _______,
+      _______,
+      _______, _______, _______,
 
-       KC_SLEP, LOCK,    _______, _______, KC_PSCR, _______, _______,
-       H(F),    H(E),    KC_VOLD, KC_MUTE, KC_VOLU, _______, _______,
-                H(A),    H(B),    H(C),    H(D),    _______, _______,
-       H(G),    XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
-                         _______, _______, _______, _______, _______,
-       _______, _______,
-       _______,
-       _______, KC_ACL0, KC_ACL1
-),
+      KC_SLEP, LOCK,    _______, _______, KC_PSCR, _______, _______,
+      H(F),    H(E),    KC_VOLD, KC_MUTE, KC_VOLU, _______, _______,
+      H(A),    H(B),    H(C),    H(D),    _______, _______,
+      H(G),    XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
+      _______, _______, _______, _______, _______,
+      _______, _______,
+      _______,
+      _______, KC_ACL0, KC_ACL1
+      ),
 
 
 /* Keymap 3: Misc layer(Right hand keys for window control)
@@ -170,25 +169,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   **   | Prev | Play | Next | PgDn |      |      |     |      |G_Lft |G_Dwn | G_Up |G_Rht |      |        |
  * `--------+------+------+------+------+-------------'     `-------------+------+------+------+------+--------'
  */
-[MISC] = KEYMAP(
-       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
-       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-       _______, KC_BSPC, KC_DELT, KC_WBAK, KC_PGUP, KC_WFWD,
-       _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_PGDN, _______, LALT(KC_F4),
-       _______, _______, _______, _______, _______,
-                                                    _______, _______,
-                                                             _______,
-                                           _______, _______, _______,
+  [MISC] = KEYMAP(
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
+      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+      _______, KC_BSPC, KC_DELT, KC_WBAK, KC_PGUP, KC_WFWD,
+      _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_PGDN, _______, LALT(KC_F4),
+      _______, _______, _______, _______, _______,
+      _______, _______,
+      _______,
+      _______, _______, _______,
 
-       _______, M(M_HW), M(M_AMA),M(M_DKV),_______, _______, _______,
-       _______, CA(LEFT),CA(DOWN),CA(UP),  CA(RGHT),_______, _______,
-                GA(LEFT),GA(DOWN),GA(UP),  GA(RGHT),_______, _______,
-       _______, G(LEFT), G(DOWN), G(UP),   G(RGHT), _______, _______,
-                         _______, _______, _______, _______, _______,
-       _______, _______,
-       _______,
-       _______, _______, _______
-),
+      _______, M(M_HW), M(M_AMA),M(M_DKV),_______, _______, _______,
+      _______, CA(LEFT),CA(DOWN),CA(UP),  CA(RGHT),_______, _______,
+      GA(LEFT),GA(DOWN),GA(UP),  GA(RGHT),_______, _______,
+      _______, G(LEFT), G(DOWN), G(UP),   G(RGHT), _______, _______,
+      _______, _______, _______, _______, _______,
+      _______, _______,
+      _______,
+      _______, _______, _______
+      ),
 };
 
 enum {
