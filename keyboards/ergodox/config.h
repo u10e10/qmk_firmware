d9f3b6ab49c477950276c115b959d38163320a72
u10e10 <uplus.e10@gmail.com>
2017-01-24 22:54:49 +0900

    Mouse speeedup

--- keyboards/ergodox/config.h
+++ keyboards/ergodox/config.h
@@ -4,7 +4,7 @@
 #define MOUSEKEY_INTERVAL       20
 #define MOUSEKEY_DELAY          0
 #define MOUSEKEY_TIME_TO_MAX    5
-#define MOUSEKEY_MAX_SPEED      2
+#define MOUSEKEY_MAX_SPEED      3
 #define MOUSEKEY_WHEEL_DELAY 0
 
 #define TAPPING_TOGGLE  1
