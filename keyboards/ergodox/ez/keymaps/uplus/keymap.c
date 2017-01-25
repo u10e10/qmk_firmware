ea478150ad1fe77b959a4259d5b7f7876db5ecab
u10e10 <uplus.e10@gmail.com>
2017-01-25 17:15:07 +0900

    Update

--- keyboards/ergodox/ez/keymaps/uplus/keymap.c
+++ keyboards/ergodox/ez/keymaps/uplus/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
  * |Ent/Alt |   A  |   S  |   D  |   F  |   G  |------|     |------|   H  |   J  |   K  |   L  | ;/L2 | '/Ctrl |
  * |--------+------+------+------+------+------|  `   |     |  =   |------+------+------+------+------+--------|
- * | Esc/L3 |   Z  |   X  |   C  |   V  |   B  |      |     |      |   N  |   M  |   ,  |   .  |   /  | \/SFT  |
+ * |  Esc   |   Z  |   X  |   C  |   V  |   B  |      |     |      |   N  |   M  |   ,  |   .  |   /  | \/SFT  |
  * |--------+------+------+------+------+-------------'     `-------------+------+------+------+------+--------|
  * |        | Gui  | Alt  | ESC  |  L3  |                                 |   [  |   ]  | Alt  | Gui  |        |
  * `------------------------------------'                                 `------------------------------------'
@@ -76,17 +76,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       MO(MISC),       KC_1,     KC_2,    KC_3,    KC_4,   KC_5,  DYN_REC1,
       KC_TAB,         KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,  KC_TAB,
       ALT_T(KC_ENT),  KC_A,     KC_S,    KC_D,    KC_F,   KC_G,
-      LT_3(ESC),      KC_Z,     KC_X,    KC_C,    KC_V,   KC_B,  KC_GRV,
+      KC_ESC,         KC_Z,     KC_X,    KC_C,    KC_V,   KC_B,  KC_GRV,
       XXXXXXX,        KC_LGUI,  KC_LALT, KC_ESC,  MO(MISC),
-      KC_LANG1,      KC_MPLY,
+      KC_LANG1,       KC_MPLY,
       KC_LANG1,
       KC_LCTL, OSM(MOD_LSFT), KC_LANG1,
 
-      DYN_PLY1,    KC_6,   KC_7,     KC_8,    KC_9,    KC_0,           XXXXXXX,
-      KC_MINS,     KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,           KC_BSPC,
-      KC_H,   KC_J,     KC_K,    KC_L,    LT_2(SCLN),     CTL_T(KC_QUOT),
-      KC_EQL,      KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH,        SFT_T(KC_BSLS),
-      KC_LBRC,  KC_RBRC, KC_RALT, KC_RGUI,        XXXXXXX,
+      DYN_PLY1,    KC_6,   KC_7,     KC_8,    KC_9,    KC_0,     XXXXXXX,
+      KC_MINS,     KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,     KC_BSPC,
+      KC_H,        KC_J,   KC_K,     KC_L,    LT_2(SCLN),        CTL_T(KC_QUOT),
+      KC_EQL,      KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  SFT_T(KC_BSLS),
+      KC_LBRC,     KC_RBRC,KC_RALT,  KC_RGUI,        XXXXXXX,
       KC_APP,      KC_LANG2,
       KC_LANG2,
       KC_LANG2,    KC_SPC, MO(SYMB)
