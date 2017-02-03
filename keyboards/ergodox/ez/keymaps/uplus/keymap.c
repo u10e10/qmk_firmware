2fdc582e2e73acdf6ce750e48a2d2f6cb15c12bd
u10e10 <uplus.e10@gmail.com>
2017-02-03 11:02:06 +0900

    Cleanup and Update

--- keyboards/ergodox/ez/keymaps/uplus/keymap.c
+++ keyboards/ergodox/ez/keymaps/uplus/keymap.c
@@ -54,9 +54,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.     ,--------------------------------------------------.
  * |   L3   |   1  |   2  |   3  |   4  |   5  |DYN_R1|     |DYN_P1|   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+------+------|     |------+------+------+------+------+------+--------|
- * |  Alt   |   Q  |   W  |   E  |   R  |   T  | TAB  |     |  -   |   Y  |   U  |   I  |   O  |   P  |   -    |
+ * | Enter  |   Q  |   W  |   E  |   R  |   T  | TAB  |     |      |   Y  |   U  |   I  |   O  |   P  |   -    |
  * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
- * |Ent/Alt |   A  |   S  |   D  |   F  |   G  |------|     |------|   H  |   J  |   K  |   L  |   ;  |  '/L2  |
+ * | Enter  |   A  |   S  |   D  |   F  |   G  |------|     |------|   H  |   J  |   K  |   L  |   ;  |  '/L2  |
  * |--------+------+------+------+------+------|  `   |     |  =   |------+------+------+------+------+--------|
  * |  Esc   |   Z  |   X  |   C  |   V  |   B  |      |     |      |   N  |   M  |   ,  |   .  |   /  |   \    |
  * |--------+------+------+------+------+-------------'     `-------------+------+------+------+------+--------|
@@ -74,19 +74,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
   [BASE] = KEYMAP(  // layer 0 : default
       MO(MISC),       KC_1,     KC_2,    KC_3,    KC_4,   KC_5,  DYN_REC1,
-      KC_LALT,        KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,  KC_TAB,
-      ALT_T(KC_ENT),  KC_A,     KC_S,    KC_D,    KC_F,   KC_G,
+      KC_ENT,         KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,  KC_TAB,
+      KC_ENT,         KC_A,     KC_S,    KC_D,    KC_F,   KC_G,
       KC_ESC,         KC_Z,     KC_X,    KC_C,    KC_V,   KC_B,  KC_GRV,
       XXXXXXX,        KC_LGUI,  KC_LALT, KC_ESC,  MO(MISC),
-      KC_LANG1,       KC_MPLY,
-      KC_LANG1,
+               KC_LANG1,      KC_MPLY,
+                              KC_LANG1,
       KC_LCTL, OSM(MOD_LSFT), KC_LANG1,
 
-      DYN_PLY1,    KC_6,   KC_7,     KC_8,    KC_9,    KC_0,     XXXXXXX,
-      KC_MINS,     KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,     KC_MINS,
-                   KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN,  LT_2(QUOT),
-      KC_EQL,      KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS,
-      KC_LBRC,     KC_RBRC,KC_RALT,  KC_RGUI,        XXXXXXX,
+      DYN_PLY1,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     XXXXXXX,
+      KC_MINS,     KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_MINS,
+                   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  LT_2(QUOT),
+      KC_EQL,      KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS,
+                           KC_LBRC, KC_RBRC, KC_RALT, KC_RGUI,  XXXXXXX,
       KC_APP,      KC_LANG2,
       KC_LANG2,
       KC_LANG2,    KC_SPC, MO(SYMB)
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+------|     |------+------+------+------+------+------+--------|
  * |        |  !   |  @   |  #   |  $   |  %   |      |     |  _   |  ^   |  &   |  *   |  (   |  )   |  F11   |
  * |--------+------+------+------+------+------|      |     |      |------+------+------+------+------+--------|
- * |        |  BS  | DEL  |   {  |   }  |  ~   |------|     |------| Left | Down |  Up  | Right|  :   |   "    |
+ * |        |  BS  |      | DEL  |      |  ~   |------|     |------| Left | Down |  Up  | Right|  :   |   "    |
  * |--------+------+------+------+------+------|  ~   |     |  +   |------+------+------+------+------+--------|
  * |        |   ^  |   $  |   [  |   ]  |      |      |     |      | Home | PgDn | PgUp | End  |  ?   |   |    |
  * `--------------------------------------------------'     `--------------------------------------------------'
@@ -107,18 +107,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [SYMB] = KEYMAP(
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   _______,
       _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC, _______,
-      _______, KC_BSPC, KC_DELT, KC_LCBR, KC_RCBR,  KC_TILD,
+      _______, KC_BSPC, XXXXXXX, KC_DELT, XXXXXXX,  KC_TILD,
       _______, KC_CIRC, KC_DLR,  KC_LBRC, KC_RBRC,  XXXXXXX,  KC_TILD,
       _______, _______, _______, _______, _______,
-      _______, _______,
-      _______,
+                _______, _______,
+                         _______,
       _______,  _______, _______,
 
       _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F12,
       KC_UNDS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, KC_F11,
                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_COLN, KC_DQUO,
       KC_PLUS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_QUES, KC_PIPE,
-      _______, _______, _______,  _______, _______,
+                        KC_LCBR, KC_RCBR, _______, _______,  _______,
       _______, _______,
       _______,
       _______, _______, _______
@@ -151,9 +151,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       H(F),    H(E),    KC_VOLD, KC_MUTE, KC_VOLU, _______, _______,
                H(A),    H(B),    H(C),    H(D),    _______, _______,
       H(G),    XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
-      _______, _______, _______, _______, _______,
-      _______, _______,
-      _______,
+                        _______, _______, _______, _______, _______,
+               _______, _______,
+                        _______,
       _______, KC_ACL0, KC_ACL1
       ),
 
@@ -183,9 +183,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, CA(LEFT),CA(DOWN),CA(UP),  CA(RGHT),_______, _______,
                GA(LEFT),GA(DOWN),GA(UP),  GA(RGHT),_______, _______,
       _______, G(LEFT), G(DOWN), G(UP),   G(RGHT), _______, _______,
-      _______, _______, _______, _______, _______,
-      _______, _______,
-      _______,
+                        _______, _______, _______, _______, _______,
+               _______, _______,
+                        _______,
       _______, _______, _______
       ),
 };
