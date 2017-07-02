7ff631266740620867932c10ece96ea4d1933cd3
uplus <uplus.e10@gmail.com>
2017-07-02 18:17:41 +0900

    Update

--- keyboards/ergodox/config.h
+++ keyboards/ergodox/config.h
@@ -3,11 +3,12 @@
 
 #define MOUSEKEY_INTERVAL       20
 #define MOUSEKEY_DELAY          0
-#define MOUSEKEY_TIME_TO_MAX    5
-// #define MOUSEKEY_TIME_TO_MAX    60
-#define MOUSEKEY_MAX_SPEED      5
-// #define MOUSEKEY_MAX_SPEED      7
 #define MOUSEKEY_WHEEL_DELAY 0
+
+// 最大速度までの時間
+#define MOUSEKEY_TIME_TO_MAX    15
+// #define MOUSEKEY_MAX_SPEED      5
+#define MOUSEKEY_MAX_SPEED      7
 
 #define TAPPING_TOGGLE  1
 
