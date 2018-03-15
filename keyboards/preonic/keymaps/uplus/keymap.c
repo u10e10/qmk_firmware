eaef06ab0185b4f12bbc9cfde098c447a5bc92ec
uplus <uplus.e10@gmail.com>
2018-03-16 00:58:58 +0900

    update

--- keyboards/preonic/keymaps/uplus/keymap.c
+++ keyboards/preonic/keymaps/uplus/keymap.c
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    C_T(MINUS)},
   {C_T(ENT),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT_M(QUOT)},
   {KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LT_A(BSLS)},
-  {KC_LANG1,L_A,     XXXXXXX, XXXXXXX, O(LSFT), KC_SPC,         KC_SPC,  L_S,     KC_EQL,  KC_LALT, KC_RGUI, KC_LANG2}
+  {KC_LANG1,KC_LGUI, KC_LALT, XXXXXXX, O(LSFT), KC_SPC,         KC_SPC,  L_S,     KC_EQL,  KC_LALT, KC_RGUI, KC_LANG2}
 },
 
 /* Sub
