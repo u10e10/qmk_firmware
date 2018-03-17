7aa765c74c0d6b971fcde30d401a418eb7126fb8
uplus <uplus.e10@gmail.com>
2018-03-17 13:30:37 +0900

    update

--- keyboards/preonic/keymaps/uplus/keymap.c
+++ keyboards/preonic/keymaps/uplus/keymap.c
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = {
   {KC_APP,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX},
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    C_T(MINUS)},
+  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINUS},
   {C_T(ENT),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT_M(QUOT)},
   {KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LT_A(BSLS)},
   {KC_LANG1,KC_LGUI, KC_LALT, XXXXXXX, O(LSFT), KC_SPC,         KC_SPC,  L_S,     KC_EQL,  KC_LALT, KC_RGUI, KC_LANG2}
