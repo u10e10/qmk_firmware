6205278bd0ecfd13cf389ae709d9a8ebd269692a
uplus <uplus.e10@gmail.com>
2018-03-25 22:20:59 +0900

    update keymap

--- keyboards/preonic/keymaps/uplus/keymap.c
+++ keyboards/preonic/keymaps/uplus/keymap.c
@@ -106,11 +106,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_APP,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX},
+  {KC_APP,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    DYN_STOP},
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINUS},
   {C_T(ENT),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT_M(QUOT)},
   {KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LT_A(BSLS)},
-  {KC_LANG1,KC_LGUI, KC_LALT, XXXXXXX, O(LSFT), KC_SPC,         KC_SPC,  L_S,     KC_EQL,  KC_LALT, KC_RGUI, KC_LANG2}
+  {KC_MPLY, KC_LGUI, KC_LALT, KC_GRV,  O(LSFT), KC_SPC,         KC_SPC,  L_S,     KC_EQL,  KC_RALT, KC_RGUI, KC_ENT}
 },
 
 /* Sub
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADVANCE] = {
-  {RESET,   _______, _______, QWERTY,  COLEMAK, DVORAK,         _______, _______, _______, _______, _______, _______},
+  {RESET,   _______, _______, QWERTY,  COLEMAK, DVORAK,         _______, _______, _______, _______, _______, KC_SLEP},
   {DEBUG,   _______, DYN_PLY1,DYN_PLY2,_______, _______,        _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______},
   {__XXX__, C(ENT),  DYN_REC1,DYN_REC2,CA(T),   A(F4),          H(A),    H(B),    H(C),    H(D),    H(G),    __XXX__},
   {TERM_ON, CA(S),   DYN_STOP,CA(D),   CA(Y),   CA(B),          H(E),    KC_MPRV, KC_MPLY, KC_MNXT, H(F),    __XXX__},
