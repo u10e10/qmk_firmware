67e24f09fc547338e2edddac3802f2f9f9c12629
uplus <uplus.e10@gmail.com>
2018-07-13 22:57:58 +0900

    update

--- keyboards/ergodox_ez/keymaps/uplus/keymap.c
+++ keyboards/ergodox_ez/keymaps/uplus/keymap.c
@@ -94,8 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
             DYN_REC1,   KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     DYN_STOP,
             KC_MINS,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_MINS,
-            KC_H,       KC_J,     KC_K,    KC_L,    KC_SCLN, LT_2(QUOT),
-            KC_EQL,     KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS,
+                        KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN,  LT_2(QUOT),
+            KC_EQL,     KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  LT_3(BSLS),
             KC_LBRC,    KC_RBRC,  KC_LALT, KC_RGUI, KC_ENT,
             KC_APP,     KC_LANG2,
             KC_LANG2,
@@ -178,8 +178,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MISC] = KEYMAP(
             _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,
             _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
-            _______, KC_BSPC, KC_DELT, KC_PGUP, XXXXXXX, XXXXXXX,
-            _______, KC_WBAK, KC_PGDN, KC_WFWD, XXXXXXX, XXXXXXX, LALT(KC_F4),
+            _______, KC_BSPC, KC_DELT, KC_PGUP, CA(T),   XXXXXXX,
+            _______, KC_WBAK, KC_PGDN, KC_WFWD, CA(Y),   XXXXXXX, LALT(KC_F4),
             _______, _______, _______, _______, _______,
             _______, _______,
             _______,
