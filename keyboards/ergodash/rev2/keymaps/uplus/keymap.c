282de0522e7f19fb8e6d84a7ef16a20be69813d0
uplus <uplus.e10@gmail.com>
2019-04-29 14:37:00 +0900

    update

--- keyboards/ergodash/rev2/keymaps/uplus/keymap.c
+++ keyboards/ergodash/rev2/keymaps/uplus/keymap.c
@@ -58,20 +58,20 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Main layer
     [_QWERTY] = LAYOUT(
-            KC_APP,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                   KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    DYN_STOP,
-            KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                   KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINUS,
+            KC_APP,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,                   XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    DYN_STOP,
+            KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TAB,                    KC_EQL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINUS,
             C_T(ENT),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                   KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, L_M(QUOT),
-            L_M(ESC),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,                   KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, L_A(BSLS),
+            L_M(ESC),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,                   XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, L_A(BSLS),
             KC_MPLY, KC_LGUI, KC_LALT, KC_GRV,           KC_LCTL, O(LSFT), KC_LCTL, LO_S,    KC_SPC,  LO_S,             KC_EQL,  KC_LALT, KC_RGUI, KC_ENT
             // tty切替に使うためにRALT -> LALT
             ),
 
     // F1~F12, Backspace, Delete, Cursor keys, Lang keys, Symbols
     [_SUB] = LAYOUT(
-            _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LPRN,                   KC_RPRN, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
-            _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LPRN,                   KC_RPRN, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F11,
+            _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX,                   XXXXXXX, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+            _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,                   _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_F11,
             __XXX__, KC_BSPC, KC_DEL,  KC_LBRC, KC_RBRC, KC_TILD, KC_LPRN,                   KC_RPRN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LANG2,KC_DQT,
-            _______, KC_EQL,  KC_PLUS, KC_LCBR, KC_RCBR, KC_GRV,  KC_LPRN,                   KC_RPRN, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_LANG1,KC_PIPE,
+            _______, KC_EQL,  KC_PLUS, KC_LCBR, KC_RCBR, KC_GRV,  XXXXXXX,                   XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_LANG1,KC_PIPE,
             _______, __XXX__, __XXX__, _______,          __XXX__, __XXX__, _______, _______, __XXX__, __XXX__,          _______, __XXX__, __XXX__, _______
             ),
 
