52961fe0750064420ceeaa2712b18138136c81bb
uplus <uplus.e10@gmail.com>
2020-12-20 14:01:50 +0900

    update

--- keyboards/moonlander/keymaps/uplus/keymap.c
+++ keyboards/moonlander/keymaps/uplus/keymap.c
@@ -75,12 +75,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Main layer
     [BASE] = LAYOUT_moonlander(
-        KC_APP,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,          XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    DM_RSTP,
+        KC_APP,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_BSPC,          XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    DM_RSTP,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TAB,           XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINUS,
         C_T(ENT),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,          KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, L_M(QUOT),
         KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, L_A(BSLS),
         KC_MPLY, KC_LGUI, KC_LALT, KC_GRV,  KC_ESC,           KC_ESC,           KC_ESC,           KC_MINUS,KC_EQL,  KC_LALT, KC_RGUI, KC_ENT,
-                                            O(LSFT), KC_LCTL, KC_LCTL,          LO_S,    LO_S,  KC_SPC
+                                            O(LSFT), KC_LCTL, KC_LCTL,          LO_S,    LO_S,    KC_SPC
     ),
 
     // F1~F12, Backspace, Delete, Cursor keys, Lang keys, Symbols
